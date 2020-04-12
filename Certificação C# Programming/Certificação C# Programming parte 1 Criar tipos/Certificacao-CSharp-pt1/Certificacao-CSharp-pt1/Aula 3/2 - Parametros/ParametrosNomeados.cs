d20f391bb8dc70b7a8eda6df17a1b9554d25553d
danielnagami <daniel.nagami@woopi.com>
2020-04-12 01:05:00 -0300

    Class 3 - Parametros Nomeados

---
+++ Certificação C# Programming/Certificação C# Programming parte 1 Criar tipos/Certificacao-CSharp-pt1/Certificacao-CSharp-pt1/Aula 3/2 - Parametros/ParametrosNomeados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Certificacao_CSharp_pt1
+{
+    class ParametrosNomeados : IAulaItem
+    {
+        public void Executar()
+        {
+            ImprimirDetalhesDoPedido("Maria de Fátima", 31, "Caneca vermelha");
+
+            ImprimirDetalhesDoPedido(numeroPedido: 31, nomeProduto: "Caneca vermelha", vendedor: "Maria de Fátima");
+
+            ImprimirDetalhesDoPedido(nomeProduto: "Caneca vermelha", vendedor: "Maria de Fátima", numeroPedido: 31);
+
+            ImprimirDetalhesDoPedido("Maria de Fátima", 31, nomeProduto: "Caneca vermelha");
+
+            ImprimirDetalhesDoPedido(vendedor: "Maria de Fátima", 31, nomeProduto: "Caneca vermelha");
+
+            ImprimirDetalhesDoPedido("Maria de Fátima", numeroPedido: 31, "Caneca vermelha");
+        }
+
+        void ImprimirDetalhesDoPedido(string vendedor, int numeroPedido, string nomeProduto)
+        {
+            if (string.IsNullOrEmpty(vendedor))
+                throw new ArgumentException(message: "Nome do vendedor não pode ser nulo ou vazio.");
+
+            Console.WriteLine($"Vendedor: {vendedor}, Pedido nº{numeroPedido}, Produto: {nomeProduto}");
+        }
+    }
+}
--- Certificação C# Programming/Certificação C# Programming parte 1 Criar tipos/Certificacao-CSharp-pt1/Certificacao-CSharp-pt1/Program.cs
+++ Certificação C# Programming/Certificação C# Programming parte 1 Criar tipos/Certificacao-CSharp-pt1/Certificacao-CSharp-pt1/Program.cs
@@ -82,7 +82,7 @@ namespace Certificacao_CSharp_pt1
                 new MenuItem("Dinâmicos", typeof(Dinamicos)),
                 new MenuItem("Strings", typeof(Strings)),
                 new MenuItem("Metodos", typeof(Metodos)),
-                //new MenuItem("Parâmetros Nomeados", typeof(ParametrosNomeados)),
+                new MenuItem("Parâmetros Nomeados", typeof(ParametrosNomeados)),
                 //new MenuItem("Parâmetros Opcionais", typeof(ParametrosOpcionais)),
                 //new MenuItem("Métodos de Extensão", typeof(MetodosDeExtensao)),
                 //new MenuItem("Propriedades Indexadas", typeof(PropriedadesIndexadas)),
