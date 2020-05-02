78f57a46eac5f4bee1d4844aebf5b1d3e2764869
unknown <daniel.nagami@gmail.com>
2020-05-01 23:21:22 -0300

    Aula 07 pt.1

---
+++ Front-end/JavaScript - Programando na linguagem da web/introducao-javascript/js/remover-paciente.js
@@ -0,0 +1,12 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.addEventListener("dblclick", function(){
+    
+})
+
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick", function(){
+//         this.remove();
+//     });
+// });
