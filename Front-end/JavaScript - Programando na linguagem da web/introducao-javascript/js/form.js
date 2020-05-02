4a1cf678e97e10cc8c228de2fdc3521dcf1db9f7
unknown <daniel.nagami@gmail.com>
2020-05-02 20:49:18 -0300

    JavaScript course final

--- Front-end/JavaScript - Programando na linguagem da web/introducao-javascript/js/form.js
+++ Front-end/JavaScript - Programando na linguagem da web/introducao-javascript/js/form.js
@@ -3,10 +3,7 @@ botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
-
     var paciente = obtemPacienteDoFormulario(form);
-
-    var pacienteTr = montaTr(paciente);
 
     var erros = validaPaciente(paciente);
 
@@ -15,14 +12,19 @@ botaoAdicionar.addEventListener("click", function (event) {
         return;
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
+    adicionaPacienteNaTabela(paciente);
 
     form.reset();
 
     var mensagemDeErro = document.querySelector("#mensagens-erro");
     mensagemDeErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function obtemPacienteDoFormulario(form) {
 
